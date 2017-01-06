2c9bdf5e90022c047710a052fc64da27ee95b0c0
mwbullen <mwbullen@gmail.com>
2017-01-06 16:20:23 -0500

    Initial commit

---
+++ Assets/NodeLayout.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeLayout : MonoBehaviour {
+	public GameObject nodePrefab;
+	public GameObject characterPrefab;
+	public GameObject characterObject;
+
+	public GameObject nodesParent;
+
+	public int numberNodesStart;
+
+	public float minX = 0;
+	public float maxX = 300;
+	public float minY = 0;
+	public float maxY = 300;
+	public float minZ = 0;
+	public float maxZ = 300;
+
+	// Use this for initialization
+	void Start () {
+		createNodes ();
+
+		createCharacter ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void createCharacter() {
+		//GameObject characterObject = GameObject.Instantiate (characterPrefab);
+
+		GameObject[] nodes = GameObject.FindGameObjectsWithTag ("Node");
+
+		GameObject randomNode = nodes [Random.Range (0, nodes.Length)];
+
+		characterObject.transform.position = randomNode.transform.position;
+	}
+
+	void createNodes() {
+		for (int i = 0; i < numberNodesStart; i++) {
+			GameObject newNode = GameObject.Instantiate (nodePrefab);
+
+			newNode.transform.position = getRandomVector ();
+			newNode.transform.parent = nodesParent.transform;
+		}
+	}
+
+	Vector3 getRandomVector() {
+		float x = Random.Range (minX, maxX);
+		float y = Random.Range (minY, maxY);
+		float z = Random.Range (minZ, maxZ);
+
+		return new Vector3 (x, y, z);
+	}
+}
---
+++ Assets/characterAction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterAction : MonoBehaviour {
+
+	public float linkDistanceLimit = 1000f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			//Debug.Log ("click");
+
+			attemptLinkNode ();
+		}
+
+	}
+
+	void attemptLinkNode() {
+
+		RaycastHit r;
+
+		Vector3 sourcePosition = Camera.main.transform.position;
+
+		Debug.DrawRay (sourcePosition, transform.forward * linkDistanceLimit);
+
+		if (Physics.Raycast(new Ray(transform.position, transform.forward), out r, linkDistanceLimit)) {
+			Debug.Log (r.collider.gameObject.tag);
+			Debug.Log("distance:  " + Vector3.Distance(transform.position, r.collider.gameObject.transform.position));
+		}
+	}
+}
